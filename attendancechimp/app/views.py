519f972d045f4e062825a91d02c70f10372a4da9
Jason Yu <Jason2024@uchicago.edu>
2023-03-24 14:06:56 -0500

    Almost there

--- attendancechimp/app/views.py
+++ attendancechimp/app/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'app/index.html', {})
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+def results(request, question_id):
+    response = "You're looking at the results of questoin %s."
+    return HttpResponse(response % question_id)
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
